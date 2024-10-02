c722ccdaca91c6418a618daf20a9598dbfbd616f
Riva N <rivarahim@gmail.com>
2024-10-02 21:53:55 +0700

    Initial commit

---
+++ 1. Two Sum/Solution.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result = {0, 0};
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.at(i) + nums.at(j) == target) {
+                    result.at(0) = i;
+                    result.at(1) = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
---
+++ 1. Two Sum/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Solution.cpp>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}

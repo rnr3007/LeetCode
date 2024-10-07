873d415d538f5563daa0b3cc2e60c7ca1e3809c5
Riva N <rivarahim@gmail.com>
2024-10-07 09:53:22 +0700

    Solving Problem #26

---
+++ 26. Remove Duplicates from Sorted Array/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <Solution.cpp>
+
+using namespace std;
+
+int main() {
+    Solution s;
+    vector<int> nums = {1, 2, 2, 3};
+    s.removeDuplicates(nums);
+    return 0;
+}

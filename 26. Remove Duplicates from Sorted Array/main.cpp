32482913ddd0107d8a5c95bf289742efb0eb6ee0
Riva N <rivarahim@gmail.com>
2024-10-07 10:02:05 +0700

    Solving Problem #26

--- 26. Remove Duplicates from Sorted Array/main.cpp
+++ 26. Remove Duplicates from Sorted Array/main.cpp
@@ -7,6 +7,11 @@ using namespace std;
 int main() {
     Solution s;
     vector<int> nums = {1, 2, 2, 3};
-    s.removeDuplicates(nums);
+    int k = s.removeDuplicates(nums);
+
+    cout << "k: " << k << "\n";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
     return 0;
 }
